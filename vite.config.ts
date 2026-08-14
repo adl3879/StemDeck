6141ac85c378f21a17ef0bddc990b62d84af23e9
Toyosi Adekanmbi <57632354+adl3879@users.noreply.github.com>
2026-08-14 13:55:12 +0100

    favicon

--- vite.config.ts
+++ vite.config.ts
@@ -21,17 +21,7 @@ export default defineConfig({
             sizes: "any",
             type: "image/svg+xml",
             purpose: "any maskable",
-          },
-          {
-            src: "icon-96.png",
-            sizes: "96x96",
-            type: "image/png",
-          },
-          {
-            src: "icon-512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
+          } 
         ],
       },
     }),
